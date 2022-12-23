a3f6d23473a63f57ca956b2a6e5cea9b23a473f5
cuonglh1 <45034579+cuonglh1@users.noreply.github.com>
2022-12-23 17:05:57 +0700

    commit code
    commit code

---
+++ client/src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = `https://9b1zn2rfu9.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-7rbgqf4p8gvme86c.us.auth0.com',
+  clientId: 'pjk3IJdOsgtQhG16kA0UkcUpKuVYZdLe',
+  callbackUrl: 'http://localhost:3000/callback'
+}
